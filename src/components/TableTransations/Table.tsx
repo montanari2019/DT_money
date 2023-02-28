680940ac2c71a69b219d9c066623e444b44727f4
@ikaromontanarib3 <desenvolvimento4@grupoib3.com.br>
2023-02-28 12:01:40 -0400

    tela base criada e estilizada

---
+++ src/components/TableTransations/Table.tsx
@@ -0,0 +1,69 @@
+import { SearchForm } from "./components/SearchForm/SearchForm";
+import { PriceHighLight, TableTransationsContainer, TransationsTable } from "./styled";
+
+export function TableTransations() {
+  return (
+    <TableTransationsContainer>
+        <SearchForm/>
+      <TransationsTable>
+        <tbody>
+          <tr>
+            <td width="50%">Desenvolvimento de sites</td>
+            <td>
+              <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+            </td>
+            <td>Venda</td>
+            <td>13/12/2022</td>
+          </tr>
+
+          <tr>
+            <td width="50%">Hamburguer</td>
+            <td>
+              <PriceHighLight variant="outcome">-R$ 50,00</PriceHighLight>
+            </td>
+            <td>Venda</td>
+            <td>25/12/2022</td>
+          </tr>
+
+          <tr>
+            <td width="50%">Hamburguer</td>
+            <td>
+              <PriceHighLight variant="outcome">-R$ 50,00</PriceHighLight>
+            </td>
+            <td>Venda</td>
+            <td>25/12/2022</td>
+          </tr>
+
+          <tr>
+            <td width="50%">Hamburguer</td>
+            <td>
+              <PriceHighLight variant="outcome">-R$ 50,00</PriceHighLight>
+            </td>
+            <td>Venda</td>
+            <td>25/12/2022</td>
+          </tr>
+
+          <tr>
+            <td width="50%">Hamburguer</td>
+            <td>
+              <PriceHighLight variant="outcome">-R$ 50,00</PriceHighLight>
+            </td>
+            <td>Venda</td>
+            <td>25/12/2022</td>
+          </tr>
+
+          <tr>
+            <td width="50%">Hamburguer</td>
+            <td>
+              <PriceHighLight variant="outcome">-R$ 50,00</PriceHighLight>
+            </td>
+            <td>Venda</td>
+            <td>25/12/2022</td>
+          </tr>
+        </tbody>
+
+        
+      </TransationsTable>
+    </TableTransationsContainer>
+  );
+}
--- src/pages/Transations/Transations.tsx
+++ src/pages/Transations/Transations.tsx
@@ -1,11 +1,14 @@
 import { Header } from "../../components/Header/Header";
 import { Summary } from "../../components/Summary/Summary";
+import { TableTransations } from "../../components/TableTransations/Table";
 
 export function Transations(){
     return(
         <div>
             <Header/>
             <Summary/>
+            
+            <TableTransations/>
         </div>
     )
 }
